f6873296c7b52a5ca614c90b25a9862f53c84986
nadarach <hn_rachedi@esi.dz>
2024-09-21 18:34:40 +0100

    Make quantity selector interactive & display 'Added' message

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,3 +1,5 @@
+import { cart } from '../data/cart.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -41,7 +43,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -54,13 +56,72 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+let timeoutId;
 
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset;
     let cartQuantity = 0;
     let matchingItem;
+    const messageElement = document.querySelector(`.js-added-to-cart-${productId}`);
+    
     const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+    cart.forEach(item => {
+      if (productId === item.productId){
+        matchingItem = item;
+      }
+    });
+  
+    if (matchingItem){
+      matchingItem.productQuantity += productQuantity;
+    } else {
+      cart.push({
+        productId,
+        productQuantity
+      });
+    }
+
+    if(!messageElement.classList.contains('is-added')){
+      messageElement.classList.add('is-added');
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => messageElement.classList.remove('is-added'), 1500);
+
+    }
+  
+    cart.forEach(item => {
+      cartQuantity += item.productQuantity;
+    });
+
+    console.log(cart);
+    
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  });
+
+});
+
+
+
+/*
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset;
+    const messageElement = document.querySelector(`.js-added-to-cart-${productId}`);
+
+    let matchingItem;
+
+    if (!messageElement.classList.contains('is-added')) {
+      messageElement.classList.add('is-added');
+
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(() => messageElement.classList.remove('is-added'), 1500);
+    }
+
+    const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+    console.log(productQuantity, cartQuantity);
 
     cart.forEach(item => {
       if(productId === item.productId) {
@@ -72,16 +133,19 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       matchingItem.quantity += productQuantity;
     } else {
       cart.push({
-        productId : productId,
-        quantity : productQuantity
+        productId,
+        productQuantity
       });
     }
+
     
     cart.forEach((item) => {
-      cartQuantity += item.quantity;
+      cartQuantity += item.productQuantity;
     });
+
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
   });
 });
+*/
