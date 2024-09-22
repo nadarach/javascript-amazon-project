b2f13d721eddc5ce9a8ee6e1d14af3f9248bfa93
nadarach <hn_rachedi@esi.dz>
2024-09-22 14:59:12 +0100

    delete products from page when clicking 'Delete'

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach(cartItem => {
   });
 
   cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -100,6 +100,9 @@ document.querySelectorAll('.js-delete-quantity-link').forEach(link => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     deleteFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.remove();
   });
 });
