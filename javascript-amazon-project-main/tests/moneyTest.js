b0f04c93feffb015f24a24cfc813d247fb14c5cc
nadarach <hn_rachedi@esi.dz>
2024-09-25 19:33:47 +0100

    rename tests-jasmine folder to tests

--- javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -1,27 +1,15 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
-console.log('Test suite : formatCurrency');
-console.log('Converts cents into dollars :');
+describe('Test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
 
-if(formatCurrency(2095) === '20.95'){
-  console.log('Passed');
-} else {
-  console.log('Failed');
-}
+  it('works with zero (0)', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
 
-console.log('Works with 0 :');
-
-if(formatCurrency(0) === '0.00'){
-  console.log('Passed');
-} else {
-  console.log('Failed');
-}
-
-console.log('Rounds up to the nearest cent : ');
-
-if(formatCurrency(2000.5) === '20.01'){
-  console.log('Passed');
-} else {
-  console.log('Failed', formatCurrency(2000.5));
-}
-
+  it('rounds up to the nearest cent', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+});
