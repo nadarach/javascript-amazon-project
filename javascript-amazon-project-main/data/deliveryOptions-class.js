7aa6c2554986086efd630b3a1626734f5271816e
nadarach <hn_rachedi@esi.dz>
2024-09-26 19:22:46 +0100

    create products-class and deliveryOptions-class (to work on converting into oop)

---
+++ javascript-amazon-project-main/data/deliveryOptions-class.js
@@ -0,0 +1,57 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js' //default export
+import isWeekend from '../scripts/utils/time.js';
+
+export const deliveryOptions = [{
+  id: '1',
+  deliveryDays : 7,
+  priceCents: 0
+}, {
+  id: '2',
+  deliveryDays : 3,
+  priceCents: 499
+}, {
+  id: '3',
+  deliveryDays : 1,
+  priceCents: 999
+}];
+
+export function getDeliveryOption(deliveryOptionId){
+  let deliveryOption; 
+
+  //looping through the delivery options to retrieve the option associated with the item in the cart
+  deliveryOptions.forEach(option => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+      return option;
+    }
+  });
+  return deliveryOption;
+}
+
+export function checkDeliveryOption(deliveryOptionId){
+  let deliveryOption = null; 
+
+  deliveryOptions.forEach(option => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+  return deliveryOption;
+}
+
+//Calculate the delivery date and format it (Day-of-week, month day-of-month format) 
+export function calculateDeliveryDate(deliveryOption){
+  let deliveryDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+  
+  while(deliveryDays > 0) {
+    deliveryDate = deliveryDate.add(1, 'days');
+    
+    if (!isWeekend(deliveryDate)) {
+      deliveryDays--;
+    } 
+  }
+
+  const dateString = deliveryDate.format('dddd, MMMM D');
+  return dateString;
+}
--- javascript-amazon-project-main/data/products-class.js
+++ javascript-amazon-project-main/data/products-class.js
@@ -1,5 +1,51 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML(){
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super (productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `;
+  }
+}
+
 export function getProduct(productId){
   let matchingProduct;
 
@@ -672,4 +718,9 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  } 
+  return new Product(productDetails);
+});
